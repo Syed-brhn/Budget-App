4330cbba76adb94d83ebda27ea0d460cff5a06b0
Syed Burhan Ali <syed.burhan8005@gmail.com>
2023-06-03 23:47:01 +0500

    Bootstrap
    Added Bootstrap also getting value from input

---
+++ app.js
@@ -0,0 +1,14 @@
+const addExpense = () => {
+
+    const expenseTitle = document.getElementById("expenseTitle").value
+    const expenseCost = document.getElementById("expenseCost").value
+
+    console.log("expenseTitle", expenseTitle)
+    console.log("expenseCost", expenseCost)
+}
+const addbudget = () => {
+
+    const expenseTitle = document.getElementById("budgetCost").value
+
+    console.log("expenseCost", expenseTitle)
+}
